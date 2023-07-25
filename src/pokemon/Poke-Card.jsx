05c2d40915dbd92173ee0327ea6498c10763a78a
Cristobal Rivera Moreno <kris_todosinfin@outlook.com>
2023-07-24 19:22:44 -0600

    Colores y mejoras

--- src/pokemon/Poke-Card.jsx
+++ src/pokemon/Poke-Card.jsx
@@ -4,7 +4,7 @@ export function PokeCard({pokemon,infoPokemon}){
 
 
     return (
-        <div className="flex row bg-red-400 rounded-lg mr-2 mb-2" onClick={()=>infoPokemon(pokemon)}>
+        <div className="flex row bg-red-400 hover:bg-red-500 rounded-lg mr-2 mb-2" onClick={()=>{infoPokemon(pokemon);document.getElementById('more-info').classList.remove('hidden')}}>
             <img  src={pokemon.sprites.front_default}  alt="" />
             <h1 className="border-l-4 border-zinc-700 text-2xl p-8 ">
                 {pokemon.name}</h1>
